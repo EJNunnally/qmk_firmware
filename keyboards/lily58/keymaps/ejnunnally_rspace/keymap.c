e03111c5cb0144cd0907c3bfed9ff5b4b5746991
erik <ejnunnally@gmail.com>
2025-12-30 15:46:41 -0700

    modified rspace lily58

--- keyboards/lily58/keymaps/ejnunnally_rspace/keymap.c
+++ keyboards/lily58/keymaps/ejnunnally_rspace/keymap.c
@@ -12,19 +12,19 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT( // The right space is so I can see if I actually type faster
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,  KC_SPC, KC_BSPC, KC_ENT, MO(_RAISE)
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,  KC_SPC,  KC_ENT, KC_DEL, MO(_RAISE)
 ),
 
 [_LOWER] = LAYOUT( // I need the 3 key for Minecraft's F3 + 3 hotkey
    KC_F11,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
   _______, XXXXXXX, XXXXXXX,    KC_3, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL, XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX,  KC_GRV, KC_MINS,  KC_EQL, XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______, _______, _______, KC_DEL, _______, _______
+                             _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT(
