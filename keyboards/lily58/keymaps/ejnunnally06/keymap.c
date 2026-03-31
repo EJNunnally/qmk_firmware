71a27b1ea791e6c86237035f42a8631f6cac69bb
erik <ejnunnally@gmail.com>
2026-03-31 11:09:17 -0600

    added qwerty toggle above base layer

--- keyboards/lily58/keymaps/ejnunnally06/keymap.c
+++ keyboards/lily58/keymaps/ejnunnally06/keymap.c
@@ -1,55 +1,38 @@
 #include QMK_KEYBOARD_H
 
+// to change keymap, use
+// make lily58:(any folder):avrdude
+
 enum layer_number {
     BSE = 0,
-    QWE,
     LWR,
-    RSE,
-    LAY
+    RSE
 };
-
-#define LT_ESL LT(LAY, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BSE] = LAYOUT(
-            LT_ESL,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+            KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
             KC_TAB,  KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_BSLS,
             KC_LCTL,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,
             KC_LSFT, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X, KC_SLSH,  KC_EQL,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
             KC_LALT, KC_LGUI, MO(LWR),  KC_SPC,  KC_ENT, KC_BSPC,  KC_DEL,  MO(RSE)
             ),
 
-    [QWE] = LAYOUT(
-            LT_ESL,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-            KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
-            KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-            KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MINS,  KC_EQL,    KC_N,    KC_M,  KC_DOT, KC_COMM, KC_SLSH, KC_RSFT,
-            KC_LALT, KC_LGUI, MO(LWR),  KC_SPC,  KC_ENT, KC_BSPC,  KC_DEL,  MO(RSE)
-            ),
-
-    [LWR] = LAYOUT(
-            KC_F11,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
+    [LWR] = LAYOUT( // I need the 3 key for Minecraft's F3 + 3 hotkey
+             KC_F11,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
             _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
             _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, KC_CAPS,
             _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
             _______, _______, _______, _______, _______, _______, _______, _______
             ),
 
-    [RSE] = LAYOUT(
-            KC_ESC,  XXXXXXX, XXXXXXX, MS_WHLU, XXXXXXX, XXXXXXX,                    KC_INS, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, XXXXXXX,
+    [RSE] = LAYOUT( // stupid Windows and their ALT codes
+            _______, XXXXXXX, XXXXXXX, MS_WHLU, XXXXXXX, XXXXXXX,                    KC_INS, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, XXXXXXX,
             _______, XXXXXXX, XXXXXXX,   MS_UP, XXXXXXX, MS_ACL0,                    KC_NUM,   KC_P7,   KC_P8,   KC_P9, XXXXXXX, XXXXXXX,
             _______, MS_BTN2, MS_LEFT, MS_DOWN, MS_RGHT, MS_ACL1,                   XXXXXXX,   KC_P4,   KC_P5,   KC_P6, XXXXXXX, KC_CAPS,
             _______, XXXXXXX, XXXXXXX, MS_WHLD, XXXXXXX, MS_ACL2, MS_BTN3, _______,   KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, _______,
             _______, _______, _______, MS_BTN1, _______, _______, _______, _______
-            ),
-
-    [LAY] = LAYOUT( // esc-1 is qwerty, esc-2 is dvorak
-            _______, TO(QWE), TO(BSE), XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
             )
 
 };
@@ -70,17 +53,11 @@ bool oled_task_user(void) {
             case BSE:
                 oled_write_P(PSTR("     \n"), false);
                 break;
-            case QWE:
-                oled_write_P(PSTR("QWERT\n"), false);
-                break;
             case LWR:
                 oled_write_P(PSTR("LOWER\n"), false);
                 break;
             case RSE:
                 oled_write_P(PSTR("RAISE\n"), false);
-                break;
-            case LAY:
-                oled_write_P(PSTR("LAYER\n"), false);
                 break;
             default:
                 // Or use the write_ln shortcut over adding '\n' to the end of your string
