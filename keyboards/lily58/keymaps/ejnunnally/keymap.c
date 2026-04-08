f6eff18323e3a56ee4e9314a6bdf62fc95df200a
erik <ejnunnally@gmail.com>
2026-04-07 20:33:29 -0600

    LT_ESL key in Raise layer

--- keyboards/lily58/keymaps/ejnunnally/keymap.c
+++ keyboards/lily58/keymaps/ejnunnally/keymap.c
@@ -13,7 +13,7 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BSE] = LAYOUT(
-            LT_ESL,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+            KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
             KC_TAB,  KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_BSLS,
             KC_LCTL,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,
             KC_LSFT, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X, KC_SLSH,  KC_EQL,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             ),
 
     [QWE] = LAYOUT(
-            LT_ESL,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+            KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
             KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
             KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
             KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MINS,  KC_EQL,    KC_N,    KC_M,  KC_DOT, KC_COMM, KC_SLSH, KC_RSFT,
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             ),
 
     [RSE] = LAYOUT(
-            KC_ESC,  XXXXXXX, XXXXXXX, MS_WHLU, XXXXXXX, XXXXXXX,                    KC_INS, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, XXXXXXX,
+            LT_ESL,  XXXXXXX, XXXXXXX, MS_WHLU, XXXXXXX, XXXXXXX,                    KC_INS, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, XXXXXXX,
             _______, XXXXXXX, XXXXXXX,   MS_UP, XXXXXXX, MS_ACL0,                    KC_NUM,   KC_P7,   KC_P8,   KC_P9, XXXXXXX, XXXXXXX,
             _______, MS_BTN2, MS_LEFT, MS_DOWN, MS_RGHT, MS_ACL1,                   XXXXXXX,   KC_P4,   KC_P5,   KC_P6, XXXXXXX, KC_CAPS,
             _______, XXXXXXX, XXXXXXX, MS_WHLD, XXXXXXX, MS_ACL2, MS_BTN3, _______,   KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, _______,
